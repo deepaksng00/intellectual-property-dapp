66da4aa4f4ddb8a2a0f41a40724cf73825240334
Deepak Singh <singh.deepak20@outlook.com>
2021-01-22 18:33:47 +0000

    Solidity contract is in working order

--- ethereum/Contracts/IntellectualProperty.sol
+++ ethereum/Contracts/IntellectualProperty.sol
@@ -11,70 +11,61 @@ contract RegisteredIPFactory {
     
     /* --- Stores all deployed designs --- */
     address[] private deployedDesigns;
-    
-    /* --- Deploys new intellectual property on the blockchain --- */
-    function createIP(string memory typeOfIP) public {
-        if (keccak256(bytes(typeOfIP)) == keccak256(bytes("TRADEMARK"))) {
-            string memory code = "TM";
-            uint256 number = deployedTrademarks.length + 1;
-            string memory id = string(abi.encodePacked(code, number));
-            Trademark newTrademark = new Trademark(id, block.timestamp, msg.sender);
-            deployedTrademarks.push(newTrademark.getAddress());
-        } else if (keccak256(bytes(typeOfIP)) == keccak256(bytes("PATENT"))) {
-            string memory code = "PT";
-            uint256 number = deployedPatents.length + 1;
-            string memory id = string(abi.encodePacked(code, number));
-            Patent newPatent = new Patent(id, block.timestamp, msg.sender);
-        } else if (keccak256(bytes(typeOfIP)) == keccak256(bytes("DESIGN"))) {
-            string memory code = "DS";
-            uint256 number = deployedDesigns.length + 1;
-            string memory id = string(abi.encodePacked(code, number));
-            Design newDesign = new Design(id, block.timestamp, msg.sender);
-        }
+
+
+    /* --- Deploys new trademark on the blockchain --- */
+    function createTrademark() public {
+        Trademark newTrademark = new Trademark(block.timestamp, msg.sender);
+        deployedTrademarks.push(address(newTrademark));
+    }
+
+    /* --- Deploys new patent on the blockchain --- */
+    function createPatent() public {
+        deployedPatents.push(address(new Patent(block.timestamp, msg.sender)));
+    }
+
+    /* --- Deploys new design on the blockchain --- */
+    function createDesign() public {
+        Design newDesign = new Design(block.timestamp, msg.sender);
+        deployedDesigns.push(address(newDesign));
     }
     
-    /* --- Returns all registered intellectual property by type --- */
-    function getRegisteredIP(string memory typeOfIP) public view returns (address[] memory) {
-        if (typeOfIP == "TRADEMARK") {
-            return deployedTrademarks;
-        } else if (typeOfIP == "PATENT") {
-            return deployedPatents;
-        } else if (typeOfIP == "DESIGN") {
-            return deployedDesigns;
-        }
+    /* --- Returns all trademarks --- */
+    function getTrademarks() public view returns (address[] memory) {
+        return deployedTrademarks;
+    }
+
+    /* --- Returns all trademarks --- */
+    function getPatents() public view returns (address[] memory) {
+        return deployedPatents;
+    }
+
+    /* --- Returns all trademarks --- */
+    function getDesigns() public view returns (address[] memory) {
+        return deployedDesigns;
     }
 }
 
 /* --- Intellectual Property parent contract --- */
 contract IntellectualProperty {
-    string private ip_id;
     string private status;
     uint256 private filingDate;
     uint256 private publicationDate;
-    address[] private owners;
+    address private owner;
     
     /* --- Modifier to restrict access only to the owners --- */
     modifier restricted() {
-        require(msg.sender == owners, "Access denied. Only the owners can access this function.");
+        require(msg.sender == owner, "Access denied. Only the owners can access this function.");
         _;
     }
     
     /* --- Constructor with multiple attributes --- */
-    constructor(string memory id, uint256 date, address owner) {
-        ip_id = id;
-        filingDate = date;
-        owners.push(owner);
+    constructor(uint256 date_input, address owner_input) public {
+        filingDate = date_input;
+        owner = owner_input;
     }
     
     /* --- SETTERS AND GETTERS --- */
-
-    function getAddress() public view returns (address) {
-        return address(this);
-    }
-    
-    function getID() public view returns(string memory) {
-        return ip_id;
-    }    
     
     function getStatus() public view returns(string memory) {
         return status;
@@ -88,8 +79,8 @@ contract IntellectualProperty {
         return publicationDate;
     }
     
-    function getOwners() public view returns(address[] memory) {
-        return owners;
+    function getOwner() public view returns(address) {
+        return owner;
     }
     
     function setStatus(string memory inputStatus) public restricted {
@@ -97,26 +88,35 @@ contract IntellectualProperty {
     }
     
     function setFilingDate(uint256 date) public restricted {
-        this.filingDate = date;
+        filingDate = date;
     }
     
     function setPublicationDate(uint256 date) public restricted {
-        this.publicationDate = date;
+        publicationDate = date;
     }
     
-    function addOwner(address owner) public restricted {
-        this.owners.push(owner);
+    function setOwner(address owner_input) public restricted {
+        owner = owner_input;
     }
 }
 
 contract Trademark is IntellectualProperty {
-
+    /* --- Constructor that links to parent contract --- */
+    constructor(uint256 date_input, address owner_input) IntellectualProperty(date_input, owner_input) public {
+        
+    }
 }
 
 contract Patent is IntellectualProperty {
-    
+    /* --- Constructor that links to parent contract --- */
+    constructor(uint256 date_input, address owner_input) IntellectualProperty(date_input, owner_input) public {
+        
+    }
 }
 
 contract Design is IntellectualProperty {
-    
+     /* --- Constructor that links to parent contract --- */
+    constructor(uint256 date_input, address owner_input) IntellectualProperty(date_input, owner_input) public {
+        
+    }
 }
