cccb27af46201f9eb728b6ddaceb24824b5ecc72
Deepak Singh <singh.deepak20@outlook.com>
2021-01-28 19:22:41 +0000

    Adding React components and semantic-ui

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f85fcd8eb5ae22dec6a6";
+/******/ 	var hotCurrentHash = "5533d907425a4f7a223e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import factory from '../ethereum/factory';
 import web3 from '../ethereum/web3';
 
+/* --- Semantic-UI --- */
+import { Header, Input, Button, Select, Icon, Segment, Label } from 'semantic-ui-react';
+
 class Index extends Component {
 	state = {
 		address: ''
@@ -13,11 +16,42 @@ class Index extends Component {
 	}
 
 	render() {
+		const options = [
+  		    { key: 'Trademark', text: 'Trademark', value: 'trademark' },
+  		    { key: 'Patent', text: 'Patent', value: 'patent' },
+  		    { key: 'Design', text: 'Design', value: 'design' },
+		];
+
+		let segment;
+		let top_container;
+		let middle_container;
+
 		if (this.state.address == "") {
-			return <div>Status: Disconnected <br /> We are unable to connect to your Metamask account, please ensure Metamask is working correctly!</div>;
+			segment = <Segment color='red'>Status: Disconnected <br /> We are unable to connect to your MetaMask wallet, please ensure MetaMask is working correctly.</Segment>;
 		} else {
-		    return <div>Status: Connected <br /> Your address: { this.state.address[0] }</div>;
+			segment = <Segment color='green'>Status: Connected <br /> Your account address: { this.state.address[0] }</Segment>;
 		}
+
+		return (
+			<section class='index-container'>
+				<div class='index-left-container'>
+					<Header as='h1'>Intellectual Property</Header>
+					<p>Welcome to the intellectual property website.</p>
+
+					<Input iconPosition='left' type='text' placeholder='Search by Address' action>
+						<input />
+						<Icon name='search' />
+						<Select compact options={options} defaultValue='trademark' />
+						<Button type='submit'>Search</Button>
+					</Input>
+					<br />
+				</div>
+
+				<div class='index-right-container'>
+					{ segment }
+				</div>
+			</section>
+		);
 	}
 }
 
