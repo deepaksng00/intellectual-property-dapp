65285d48ee3e7903b3056f72e1c758b3f11a64bd
Deepak Singh <singh.deepak20@outlook.com>
2021-02-05 19:43:09 +0000

    Installing next-routes package

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e9ed78806d584f29d072";
+/******/ 	var hotCurrentHash = "5b5aca158535b4a8a0ac";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
