3718c53c146266bf4943cbf6789d6821b3c7ce71
Deepak Singh <singh.deepak20@outlook.com>
2021-02-05 19:37:22 +0000

    Fixed HTML and CSS following Next.js and React className guidelines

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c46a6411ab5a49cbfed7";
+/******/ 	var hotCurrentHash = "e9ed78806d584f29d072";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -18,23 +18,23 @@ class Index extends Component {
 		return (
 			<Layout>
 				<div className={style.main_grid_container}>
-					<nav><a href="#">Home</a></nav>
-					<section className={style.section_welcome}>
+					<nav className={style.grid_item}><a href="#">Home</a></nav>
+					<section className={[style.section_welcome, style.grid_item].join(" ")}>
 						<h2>What is intellectual property?</h2>
 						<p>Intellectual property is something that has been created by the creator's mind, for example: algorithms, stories, paintings or music.</p>
 						<p>To ensure that your intellectual property is protected from plagiarism to commercial use by a third-party, you must register this intellectual property.</p>
 						<p>This web application allows you to register your intellectual property rights on the Ethereum blockchain, where it will be stored safely. This app also allows you to access, verify and manage your intellectual property using your Ethereum wallet.</p>
 					</section>
-					<section className={style.section_search}>
+					<section className={[style.section_search, style.grid_item].join(" ")}>
 						<h2>Search for intellectual property</h2>
 						<p>To search for an intellectual property, please enter the ethereum address of the intellectual property contract:</p>
 						<form>
 							<input type="text" placeholder="Search by ethereum address" name="search" />
 							<button type="submit">Search</button>
 						</form>
-						<a href="#">Advanced Search</a>
+						<a className={style.advanced_search} href="#">Advanced Search</a>
 					</section>
-					<section className={style.section_register}>
+					<section className={[style.section_register, style.grid_item].join(" ")}>
 						<h2>Registering your intellectual property</h2>
 						<p>To register your intellectual property, you must have the Metamask browser extension installed with an Ethereum wallet.</p>
 						<h2>To register your intellectual property you will need:</h2>
@@ -45,11 +45,7 @@ class Index extends Component {
 							<li>The publication date for the intellectual property</li>
 						</ol>
 					</section>
-					<button className={style.button_start_register}>Start registration</button>
-					<section class="grid-item attention">
-						<h3 style={{ color: "#cc3300" }}>Attention!</h3>
-						<p>Registering your intellectual property will incur fees. You will be prompted by Metamask informing you of the exact value.</p>
-					</section>
+					<button className={[style.button_start_register, style.grid_item].join(" ")}>Start registration</button>
 				</div>
 			</Layout>
 		);
