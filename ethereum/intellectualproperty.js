7bf09e589f71081169f8c4e5829da0633c9b95e3
Deepak Singh <singh.deepak20@outlook.com>
2021-03-19 15:38:15 +0000

    Added new contract address on intellectualproperty.js

--- ethereum/intellectualproperty.js
+++ ethereum/intellectualproperty.js
@@ -1,7 +1,7 @@
 import web3 from './web3';
 import IntellectualProperty from '../build/contracts/IntellectualProperty.json';
 
-const address = "0xB61cCa4A95c8c5f1744f6Cd436EbE88167d4C6a8";
+const address = "0x9BBD879555839b96ed39AE0e3e478C3854A67C44";
 const abi = IntellectualProperty.abi;
 
 const contract = new web3.eth.Contract(
