42032aca6c629dac7f3d6c56f2d8f5a9284111ff
Deepak Singh <30020456+deepaksng00@users.noreply.github.com>
2021-04-05 19:42:19 +0100

    Testing GPG push

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app)
 const {createServer} = require('http')
 
-
 app.prepare().then(() => {
   createServer(handler).listen(3000, (err) => {
     if (err) throw err;
