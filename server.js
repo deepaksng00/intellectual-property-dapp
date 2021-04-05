d3c28b9d46334c87a836cbe9b1028b6bd9bd9023
Deepak Singh <30020456+deepaksng00@users.noreply.github.com>
2021-04-05 19:31:59 +0100

    Testing GPG verified commits

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app)
 const {createServer} = require('http')
 
+
 app.prepare().then(() => {
   createServer(handler).listen(3000, (err) => {
     if (err) throw err;
