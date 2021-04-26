392c3d7c3ce72645b1496f44374aa4cd57ef5c87
Deepak Singh <30020456+deepaksng00@users.noreply.github.com>
2021-04-26 13:19:38 +0100

    Added some unit tests

--- test/intellectualproperty.test.js
+++ test/intellectualproperty.test.js
@@ -1,22 +1,42 @@
 const IntellectualProperty = artifacts.require("IntellectualProperty");
 
 contract("IntellectualProperty", accounts => {
-	it("Creates two new tokens", () =>
+	it("Deploys correctly", async () => {
+		try {
+			const instance = await IntellectualProperty.deployed();
+			assert.equal(true, true);
+		} catch (err) {
+			assert.equal(false, true);
+		}
+	});
+
+	it("Gets the correct amount of tokens assigned to a user", () =>
 		IntellectualProperty.deployed()
 			.then(instance => {
-				instance.awardIP(accounts[0], "hashexample", "metadata")
-				instance.awardIP(accounts[0], "hashexample2", "metadata1")
+				instance.awardIP(accounts[0], "hash", "metadata2")
 				return instance.balanceOf.call(accounts[0])
 			})
 			.then(numOfTokens => {
-				assert.equal(numOfTokens, 2, "The token wasn't successfully created");
+				assert.equal(numOfTokens, 1, "The number of tokens is incorrect")
+			})
+	)
+
+	it("Creates two new tokens", () =>
+		IntellectualProperty.deployed()
+			.then(instance => {
+				instance.awardIP(accounts[1], "hashexample", "metadata")
+				instance.awardIP(accounts[1], "hashexample2", "metadata1")
+				return instance.balanceOf.call(accounts[1])
+			})
+			.then(numOfTokens => {
+				assert.equal(numOfTokens, 2, "The token wasn't successfully created")
 			})
 	);
 
 	it("Can access the metadata of the token", () => 
 		IntellectualProperty.deployed()
 			.then(instance => {
-				return instance.tokenURI.call(1);
+				return instance.tokenURI.call(2)
 			})
 			.then(tokenURI => {
 				assert.equal(tokenURI, "metadata", "The metadata didn't register correctly")
@@ -25,7 +45,7 @@ contract("IntellectualProperty", accounts => {
 
 	it("Blocks multiple registration of the same hash", async () => {
 		const instance = await IntellectualProperty.deployed();
-    	const balance = await instance.tokenURI.call(1);
+    	const balance = await instance.tokenURI.call(2);
     	var passed = false;
 	    	
 	    try {
@@ -36,6 +56,16 @@ contract("IntellectualProperty", accounts => {
 	    	assert.equal(error, true, "The contract recorded a token with the same hash twice");
 	    }
 	});
+
+	it("Can retrive the owner of the token", () =>
+		IntellectualProperty.deployed()
+			.then(instance => {
+				return instance.ownerOf(1)
+			})
+			.then(ownerAddress => {
+				assert.equal(ownerAddress, accounts[0])
+			})
+	)
 
 	it("Allows to transfer IP", async () => {
 		const instance = await IntellectualProperty.deployed();
